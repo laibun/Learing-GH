06b65cebc6e82d7884f79edd60610ba4968d5450
aa <laibun22@mial.ru>
2023-05-03 13:28:55 +0300

    Initial Commit

---
+++ LerningGH/LerningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LerningGHApp.swift
+//  LerningGH
+//
+//  Created by Вадим on 3.05.23.
+//
+
+import SwiftUI
+
+@main
+struct LerningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
